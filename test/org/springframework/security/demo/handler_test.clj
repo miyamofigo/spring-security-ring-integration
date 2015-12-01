a5481c8c311b1edf47f03c66faf8f99998ec3fb5
Takeo Miyamoto <miyamofigo@gmail.com>
2015-12-01 14:32:09 +0900

    Some tests has been added.

--- test/org/springframework/security/demo/handler_test.clj
+++ test/org/springframework/security/demo/handler_test.clj
@@ -1,7 +1,10 @@
 (ns org.springframework.security.demo.handler-test
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
-            [org.springframework.security.demo.handler :refer :all]))
+            [org.springframework.security.demo.handler :refer :all])
+  (:import org.eclipse.jetty.servlet.ServletTester
+           org.eclipse.jetty.server.Server
+           org.eclipse.jetty.server.LocalConnector))
 
 (deftest test-app
   (testing "main route"
@@ -12,3 +15,27 @@
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
       (is (= (:status response) 404)))))
+
+(defn- create-server []
+  (let [server (Server.)]
+    (doto server 
+      (.addConnector (LocalConnector. server)))))
+
+(defn- destroy-server [server]
+  (doto server (.stop) (.join)))
+
+(defn- set-configurer [server]
+  (doto server (.setHandler (create-handler app))))
+
+(defn- start [server] (doto server (.start)))
+
+(deftest auth-redirect-test 
+  (let [server (create-server)
+        found? (fn [response text]
+                 (is (not (= (. response indexOf text) -1))))]
+    (doto (.getResponses
+            (-> server (set-configurer) (start) (.getConnectors) (aget 0))
+            "GET / HTTP/1.1\r\nHost: localhost\r\n\r\n")
+      (found? "302 Found")
+      (found? "Location: http://localhost/login"))
+    (destroy-server server)))
